99068890337a71a8efcf315b528d9c0743b88850
fzhedu@gmail.com <fzhedu@gmail.com>
2019-01-08 11:19:10 +0800

    github compile ok

--- src/cpu_mapping.c
+++ src/cpu_mapping.c
@@ -17,52 +17,47 @@ static int inited = 0;
 static int max_cpus;
 static int node_mapping[MAX_NODES];
 
-/** 
+/**
  * Initializes the cpu mapping from the file defined by CUSTOM_CPU_MAPPING.
  * The mapping used for our machine Intel L5520 is = "8 0 1 2 3 8 9 10 11".
  */
-static int
-init_mappings_from_file()
-{
-    FILE * cfg;
-	int i;
+static int init_mappings_from_file() {
+  FILE* cfg;
+  int i;
 
-    cfg = fopen(CUSTOM_CPU_MAPPING, "r");
-    if (cfg!=NULL) {
-        if(fscanf(cfg, "%d", &max_cpus) <= 0) {
-            perror("Could not parse input!\n");
-        }
-
-        for(i = 0; i < max_cpus; i++){
-            if(fscanf(cfg, "%d", &node_mapping[i]) <= 0) {
-                perror("Could not parse input!\n");
-            }
-        }
-
-        fclose(cfg);
-        return 1;
+  cfg = fopen(CUSTOM_CPU_MAPPING, "r");
+  if (cfg != NULL) {
+    if (fscanf(cfg, "%d", &max_cpus) <= 0) {
+      perror("Could not parse input!\n");
     }
 
+    for (i = 0; i < max_cpus; i++) {
+      if (fscanf(cfg, "%d", &node_mapping[i]) <= 0) {
+        perror("Could not parse input!\n");
+      }
+    }
 
-    /* perror("Custom cpu mapping file not found!\n"); */
-    return 0;
+    fclose(cfg);
+    return 1;
+  }
+
+  /* perror("Custom cpu mapping file not found!\n"); */
+  return 0;
 }
 
-/** 
+/**
  * Try custom cpu mapping file first, if does not exist then round-robin
- * initialization among available CPUs reported by the system. 
+ * initialization among available CPUs reported by the system.
  */
-static void 
-init_mappings()
-{
-    if( init_mappings_from_file() == 0 ) {
-        int i;
-        
-        max_cpus  = sysconf(_SC_NPROCESSORS_ONLN);
-        for(i = 0; i < max_cpus; i++){
-            node_mapping[i] = i;
-        }
+static void init_mappings() {
+  if (init_mappings_from_file() == 0) {
+    int i;
+
+    max_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+    for (i = 0; i < max_cpus; i++) {
+      node_mapping[i] = i;
     }
+  }
 }
 
 /** @} */
@@ -70,15 +65,13 @@ init_mappings()
 /**
  * Returns SMT aware logical to physical CPU mapping for a given thread id.
  */
-int 
-get_cpu_id(int thread_id) 
-{
-    if(!inited){
-        init_mappings();
-        inited = 1;
-    }
+int get_cpu_id(int thread_id) {
+  if (!inited) {
+    init_mappings();
+    inited = 1;
+  }
 
-    return node_mapping[thread_id % max_cpus];
+  return node_mapping[thread_id % max_cpus];
 }
 
 /* TODO: These are just place-holder implementations. */
@@ -95,42 +88,38 @@ static int numa[][16] = {
     {0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60},
     {1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61},
     {2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62},
-    {3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63} };
+    {3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63}};
 
-int
-get_numa_id(int mytid)
-{
+int get_numa_id(int mytid) {
 #if INTEL_E5
-    int ret = 0;
+  int ret = 0;
 
-    for(int i = 0; i < 4; i++)
-        for(int j = 0; j < 16; j++)
-            if(numa[i][j] == mytid){
-                ret = i;
-                break;
-            }
-    
-    return ret;
+  for (int i = 0; i < 4; i++)
+    for (int j = 0; j < 16; j++)
+      if (numa[i][j] == mytid) {
+        ret = i;
+        break;
+      }
+
+  return ret;
 #else
-    return 0;
+  return 0;
 #endif
 }
 
-int
-get_num_numa_regions(void)
-{
-    /* TODO: FIXME automate it from the system config. */
+int get_num_numa_regions(void) {
+/* TODO: FIXME automate it from the system config. */
 #if INTEL_E5
-    return 4;
+  return 4;
 #else
-    return 1;
+  return 1;
 #endif
 }
 
-int 
-get_numa_node_of_address(void * ptr)
-{
-    int numa_node = -1;
-    get_mempolicy(&numa_node, NULL, 0, ptr, MPOL_F_NODE | MPOL_F_ADDR);
-    return numa_node;
+int get_numa_node_of_address(void* ptr) {
+  int numa_node = -1;
+#if !KNL
+  get_mempolicy(&numa_node, NULL, 0, ptr, MPOL_F_NODE | MPOL_F_ADDR);
+#endif
+  return numa_node;
 }
