3ca3babecd0e48dedff14672869e8ede02d053ce
fzhedu@gmail.com <fzhedu@gmail.com>
2019-02-21 12:04:03 +0800

    update test.sh for D+G

--- src/prefetch.h
+++ src/prefetch.h
@@ -1,31 +1,3 @@
-/*
- * Copyright [2012-2015] DaSE@ECNU
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * /multicore-hashjoins-0.2-ICDE13/src/prefetch.h
- *
- *  Created on: Dec 18, 2018
- *      Author: claims
- *		   Email:
- *
- * Description:
- *
- */
-
 #ifndef SRC_PREFETCH_H_
 #define SRC_PREFETCH_H_
 #include <assert.h>
@@ -39,7 +11,7 @@ typedef struct StateSIMD StateSIMD;
 
 #define ScalarStateSize 1
 #define PDIS 640
-#define SIMDStateSize 5
+#define SIMDStateSize 1
 
 #define LOAD_FACTOR 1
 #define MULTI_TUPLE (BUCKET_SIZE - 1)
--- src/test.sh
+++ src/test.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-##################跑之前要根据文件prefetch.h的具体值设置下面两个变量########################
-# Expr 1: state size
-# range=1~40, +4
-oldscalarstatesize=30
-# range=1~20, +2
-oldsimdstatesize=5
-
-sstatestart=1
-sstateend=50
-statestart=1
-stateend=30
-
-
-# Expr 2: sequential prefetch distance
-oldpdis=320
-
-disstart=640
-disend=640
-#####################
-app="NPO"
-#app="PIPELINE"
-#app="BTS"
-
 #keep the same with REPEAT_PROBE
 repeat=2
 
@@ -34,8 +11,8 @@ function run_a_cycle(){
 	r_size_set=("64M")
 	#s_size_set=("6.4e+08" "3.2e+08" "1.6e+08")
 	s_size_set=("500M")
-	r_skew_set=(0.5 1)
-	s_skew_set=(0.5 1)
+	r_skew_set=(0.5)
+	s_skew_set=(0.5)
 	#thread_nums=(1 4 8 16 24 32)
 	thread_nums=(1)
 	#################NOTE: 请不要注释掉以下循环，修改参数请在上面修改###########################
@@ -60,37 +37,115 @@ function run_a_cycle(){
 	done;
 }
 
+function expr_group_size() {
+	# paramter 1: state size
+	# range=1~40, +4 scalarstatesize
+	# range=1~20, +1 simdstatesize=5
+	sstatestart=1
+	sstateend=50
+	statestart=1
+	stateend=30
+	# paramter 2: sequential prefetch distance
+	disstart=640
+	disend=640
+	# paramter 3:application name
+	app="NPO"
+	#app="PIPELINE"
+	#app="BTS"
+	dir_name="results_group"_$(date +%F-%T)
+	mkdir $dir_name
+	for((pdis=$disstart;pdis<=$disend;pdis+=64)) do
+		#############修改prefetch.h文件##################
+		sed -i "/#define\ PDIS/c\#define\ PDIS\ ${pdis}" prefetch.h
+		for((scalarstatesize=$sstatestart,simdstatesize=$statestart;scalarstatesize<=$sstateend;scalarstatesize+=2,simdstatesize+=1)) do
+		        output_file=${dir_name}/${app}_pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}.txt
+		        echo $output_file
+		        #############修改prefetch.h文件##############
+		        sed -i "/#define\ SIMDStateSize/c\#define\ SIMDStateSize\ ${simdstatesize}" prefetch.h
+		        sed -i "/#define\ ScalarStateSize/c\#define\ ScalarStateSize\ ${scalarstatesize}" prefetch.h
+			echo "pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}" >> chg_prefetch_h.log
+		        cat prefetch.h >> chg_prefetch_h.log
+		        cd ..
+		        ##########################
+		        make
+		        cd src
+		        echo "start to run..."
+		        run_a_cycle $output_file ${pdis} ${simdstatesize} ${scalarstatesize}
+		        echo "end run..."
+		        results_file=${dir_name}/${app}_results_pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}.csv
+		        echo "start to parse..."
+		        echo $output_file
+		        #############解析结果的脚本，第一个参数是源文件，第二个参数是结果文件，第三个参数是你repeat的次数################
+		        python test_results_parser.py $output_file $results_file $repeat
+			rm ${output_file}
+		        echo "end parse..."
+		done;
+	done;
+}
+function expr_pdis() {
+	# paramter 1: state size
+	# range=1~40, +4 scalarstatesize
+	# range=1~20, +1 simdstatesize=5
+	sstatestart=30
+	sstateend=30
+	statestart=5
+	stateend=5
+	# paramter 2: sequential prefetch distance
+	disstart=0
+	disend=1280
+	# paramter 3:application name
+	app="NPO"
+	#app="PIPELINE"
+	#app="BTS"
+	dir_name="results_pdis"_$(date +%F-%T)
+	mkdir $dir_name
+	for((pdis=$disstart;pdis<=$disend;pdis+=64)) do
+		#############修改prefetch.h文件##################
+		sed -i "/#define\ PDIS/c\#define\ PDIS\ ${pdis}" prefetch.h
+		for((scalarstatesize=$sstatestart,simdstatesize=$statestart;scalarstatesize<=$sstateend;scalarstatesize+=2,simdstatesize+=1)) do
+		        output_file=${dir_name}/${app}_pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}.txt
+		        echo $output_file
+		        #############修改prefetch.h文件##############
+		        sed -i "/#define\ SIMDStateSize/c\#define\ SIMDStateSize\ ${simdstatesize}" prefetch.h
+		        sed -i "/#define\ ScalarStateSize/c\#define\ ScalarStateSize\ ${scalarstatesize}" prefetch.h
+			echo "pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}" >> chg_prefetch_h.log
+		        cat prefetch.h >> chg_prefetch_h.log
+		        cd ..
+		        ##########################
+		        make
+		        cd src
+		        echo "start to run..."
+		        run_a_cycle $output_file ${pdis} ${simdstatesize} ${scalarstatesize}
+		        echo "end run..."
+		        results_file=${dir_name}/${app}_results_pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}.csv
+		        echo "start to parse..."
+		        echo $output_file
+		        #############解析结果的脚本，第一个参数是源文件，第二个参数是结果文件，第三个参数是你repeat的次数################
+		        python test_results_parser.py $output_file $results_file $repeat
+			rm ${output_file}
+		        echo "end parse..."
+		done;
+	done;
+}
+	
 
+echo "input expr name : 
+G: group size
+D: prefetch distance
+G+D: G+D
+------------------"
+read expr_name
 
-for((pdis=$disstart;pdis<=$disend;pdis+=64)) do
-        #############修改prefetch.h文件##################
-        sed -i "s/define\ PDIS\ ${oldpdis}/define\ PDIS\ ${pdis}/g" prefetch.h
-        for((scalarstatesize=$sstatestart,simdstatesize=$statestart;scalarstatesize<=$sstateend;scalarstatesize+=2,simdstatesize+=4)) do
-                output_file=results/${app}_pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}.txt
-                echo $output_file
-                #############修改prefetch.h文件##############
-                sed -i "s/define\ SIMDStateSize\ ${oldsimdstatesize}/define\ SIMDStateSize\ ${simdstatesize}/g" prefetch.h
-                sed -i "s/define\ ScalarStateSize\ ${oldscalarstatesize}/define\ ScalarStateSize\ ${scalarstatesize}/g" prefetch.h
-		echo "pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}" >> chg_prefetch_h.log
-                cat prefetch.h >> chg_prefetch_h.log
-                cd ..
-                ##########################
-                make
-                cd src
-                echo "start to run..."
-                run_a_cycle $output_file ${pdis} ${simdstatesize} ${scalarstatesize}
-                echo "end run..."
-                results_file=results/${app}_results_pdis_${pdis}_simdstatesize_${simdstatesize}_scalarstatesize_${scalarstatesize}.csv
-                echo "start to parse..."
-                echo $output_file
-                #############解析结果的脚本，第一个参数是源文件，第二个参数是结果文件，第三个参数是你repeat的次数################
-                python test_results_parser.py $output_file $results_file $repeat
-		rm ${output_file}
-                echo "end parse..."
-                oldsimdstatesize=$simdstatesize 
-		oldscalarstatesize=${oldscalarstatesize}
-        done;
-        oldpdis=${pdis}
-done;
+if [[ ${expr_name} == 'D' ]]; then 
+	expr_pdis
+elif [[ ${expr_name} == 'G' ]]; then	
+	expr_group_size
+elif [[ ${expr_name} == 'G+D' ]]; then
+	expr_group_size
+	expr_pdis
+else 
+	echo -n ${expr_name}
+	echo " not found"
+fi
 
 
